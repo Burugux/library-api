130aa64e578ff7d667e8f292834f218052aa4929
Leslie Burugu <leslieburugu@gmail.com>
2021-04-18 09:44:10 +0300

    Add swagger and postgres

--- rental_api/settings.py
+++ rental_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-pi%i^wbexs8o9v_8d&ma^j*ya6)y*_#pfjh@x+l2c5&v4fa&ag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'books',
     'rentals'
 ]
@@ -77,8 +79,15 @@ WSGI_APPLICATION = 'rental_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'library',
+        'USER': 'devuser',
+        'PORT': '5432',
+        'PASSWORD': 'devpass',
+        'TEST': {
+            'NAME': 'library_test',
+        },
     }
 }
 
