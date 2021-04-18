3171c81d596e92682b7afccd94136cfe8cb31ce3
Leslie Burugu <leslieburugu@gmail.com>
2021-04-18 17:35:01 +0300

    Add tests

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from books.models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -50,3 +51,13 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'books/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
