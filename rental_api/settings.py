7794dbe898cd6d293fcfd134736adb28c584b84e
Leslie Burugu <leslieburugu@gmail.com>
2021-04-19 06:52:20 +0300

    Update allowed hosts

--- rental_api/settings.py
+++ rental_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['limitless-spire-67788.herokuapp.com','127.0.0.1']
 
 
 # Application definition
